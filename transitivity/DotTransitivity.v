3fd8fa3bde51cde9c32a69be48b03357a3025487
Samuel Gruetter <samuel.gruetter@epfl.ch>
2014-05-09 09:50:29 +0200

    chain with ((notsel B /\ notsel C) \/ B = typ_bot \/ C = typ_top)

--- transitivity/DotTransitivity.v
+++ transitivity/DotTransitivity.v
@@ -429,8 +429,11 @@ does not hold (can have a p.X:bot..bot, and follow_ub_nil bot)
 
 (* linearize a derivation that uses transitivity *)
 
-Definition chain (G: ctx) (A D: typ): Prop :=
-   (exists B C, follow_ub G A B /\ subtyp notrans G B C /\ follow_lb G C D).
+Definition chain (G: ctx) (A D: typ): Prop := exists B C, 
+  follow_ub G A B /\
+  subtyp notrans G B C /\
+  follow_lb G C D /\
+  ((notsel B /\ notsel C) \/ B = typ_bot \/ C = typ_top).
 
 (*
 Lemma notransl_head: forall G A C,
@@ -465,6 +468,14 @@ Proof.
   inversion H; subst.
 *)  
 
+Lemma top_sub_notsel: forall G C, 
+  subtyp notrans G typ_top C -> notsel C -> C = typ_top.
+Proof.
+  introv Hs Hn.
+  inversion Hs; subst; trivial.
+  inversion Hn.
+Qed.
+
 (* prepend an oktrans to chain ("utrans0*") *)
 Lemma prepend_chain: forall G A1 A2 D,
   subtyp oktrans G A1 A2 ->
@@ -474,42 +485,69 @@ Proof.
   fix 5.
   introv Hokt Hch.
   unfold chain in *.
-  inversion Hokt; inversion H; subst.
+  inversion Hokt; inversion H; set (Hch' := Hch); 
+     destruct Hch' as [B [C [Hch1 [Hch2 [Hch3 Hch4]]]]]; subst.
   (* case refl *)
   assumption.
   (* case top *)
-  destruct Hch as [B [C [Hch1 [Hch2 Hch3]]]].
-  exists A1 C.
+  apply follow_ub_top in Hch1; subst.
+  exists A1 typ_top.
   split. apply follow_ub_nil.
-  apply follow_ub_top in Hch1. subst.
-  split. apply (subtyp_trans_notrans notsel_top H Hch2).
-  apply Hch3.
+  split. assumption.
+  assert (HCTop: C = typ_top). apply (top_sub_notsel Hch2).
+  destruct Hch4 as [[Hch41 Hch42] | [Hch4 | Hch4]].
+  assumption. discriminate Hch4. subst. apply notsel_top.
+  subst. auto.
   (* case bot *)
-  destruct Hch as [B [C [Hch1 [Hch2 Hch3]]]].
   exists typ_bot C.
   split. apply follow_ub_nil.
   split. apply (subtyp_bot G C).
-  apply Hch3.
+  split. assumption. auto.
   (* case bind *)
-  destruct Hch as [B [C [Hch1 [Hch2 Hch3]]]].
   assert (B = typ_bind l d2) by apply (follow_ub_bind Hch1); subst.
   exists (typ_bind l d1) C.
   split. apply follow_ub_nil.
   split. apply (subtyp_trans_notrans (notsel_bind _ _) H Hch2).
-  assumption.
+  split. assumption.
+  left. split. apply notsel_bind.
+  destruct Hch4 as [[Hch41 Hch42] | [Hch4 | Hch4]].
+    assumption.
+    discriminate Hch4.
+    subst. apply notsel_top.
   (* case asel_l *)
   set (IH := (prepend_chain G U A2 D H4 Hch)).
+  clear Hch1 Hch2 Hch3 Hch4 B C.
   destruct IH as [B [C [IH1 [IH2 IH3]]]].
   exists B C.
   split. 
   apply (follow_ub_cons H0 IH1).
   split; assumption.
-  (* case asel_r *) skip. (*
-  set (Hch' := Hch).
-  destruct Hch' as [B [C [Hch1 [Hch2 Hch3]]]].
+  (* case asel_r *)
   inversion Hch1; subst.
     (* case follow_ub_nil *)
-    destruct (notransl_head Hch2) as [[M [Hn [Hs Hch2']]] | Hs].
+    skip.
+
+    (* inverting Hch2 not a good idea because `subtyp oktrans` it contains are 
+       not < Hokt => cannot apply IH *
+    inversion Hch2; subst.
+      (* case subtyp_refl *)
+      exists A1 (typ_asel p L).
+      split. apply follow_ub_nil. 
+      split; assumption.
+      (* case subtyp_top *)
+      exists A1 typ_top.
+      split. apply follow_ub_nil.
+      split. apply subtyp_top. assumption. 
+      (* case subtyp_asel_l *)
+      assert (HdecEq: dec_typ S0 U0 = dec_typ S U) by apply (has_unique H3 H0).
+      injection HdecEq; intros; subst.
+      apply (prepend_chain G A1 S D H5).
+      apply (prepend_chain G S U D H4).
+      apply (prepend_chain G U C D H7). Guarded.
+      (* case subtyp_asel_r *)
+      skip.
+
+   destruct (notransl_head Hch2) as [[M [Hn [Hs Hch2']]] | Hs].
       (* case notransl_cons *)
       inversion Hs; subst.
       (* case notransl_nil *)
@@ -523,14 +561,14 @@ Proof.
 
       skip.
       (* case follow_lb_cons *)
-      skip.
+      skip.*)
     (* case follow_ub_cons *)
     apply (prepend_chain G A1 S D H5).
     apply (prepend_chain G S U D H4).
     assert (HdecEq: dec_typ Lo Hi = dec_typ S U) by apply (has_unique H6 H0).
     injection HdecEq; intros; subst.
     exists B C.
-    split. assumption. split. assumption. assumption.*)
+    split. assumption. split. assumption. split. assumption. assumption.
   (* case mode *)
   apply (prepend_chain G _ _ _ H (prepend_chain G _ _ _ H0 Hch)).
   (* case trans *)
