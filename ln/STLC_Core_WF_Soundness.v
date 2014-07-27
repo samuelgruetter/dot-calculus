3f72e08402c8e13e34b9c4598f414ef3455708bd
Samuel Gruetter <samuel.gruetter@epfl.ch>
2014-07-27 18:34:02 +0200

    some version compatibility fixes in STLC_Core_WF

--- ln/STLC_Core_WF_Soundness.v
+++ ln/STLC_Core_WF_Soundness.v
@@ -19,7 +19,7 @@ Lemma typing_weaken : forall G E F t T,
    ok (E & F & G) ->
    (E & F & G) |= t ~: T.
 Proof.
-  introv Typ. gen_eq (E & G) as H. gen G.
+  introv Typ. gen_eq H: (E & G). gen G.
   induction Typ; intros G EQ Ok; subst.
   apply* typing_var. apply* binds_weaken.
   apply_fresh* typing_abs as y. apply_ih_bind* H0.
@@ -33,7 +33,7 @@ Lemma typing_subst : forall F E t T z u U,
   E |= u ~: U ->
   (E & F) |= [z ~> u]t ~: T.
 Proof.
-  introv Typt Typu. gen_eq (E & z ~ U & F) as G. gen F.
+  introv Typt Typu. gen_eq G: (E & z ~ U & F). gen F.
   induction Typt; intros G Equ; subst; simpl subst.
   case_var.
     binds_get H0. apply_empty* typing_weaken.
@@ -66,18 +66,18 @@ Qed.
 Lemma progress_result : progress.
 Proof.
   introv Typ.
-  cuts K (value t \/ exists c, exists t0, exists t0', 
+  cuts K: (value t \/ exists c, exists t0, exists t0', 
                      red t0 t0' /\ t = ctx_of c t0).
   destruct K as [Val | [C [t1a [t1'a [Red Ctx]]]]]; subst*.
-  gen_eq (empty : env) as E. poses Typ' Typ.
-  induction Typ; intros; subst. 
-  contradictions.
+  gen_eq E: (empty : env). lets Typ': Typ.
+  induction Typ; intros; subst.
+  false (binds_empty_inv H0).
   left*.
   right. destruct~ IHTyp1 as [Val1 | [C [t1a [t1'a [Red1 Ctx1]]]]].
     destruct~ IHTyp2 as [Val2 | [C [t2a [t2a' [Red2 Ctx2]]]]].
       inversions Typ1; inversions Val1.
         exists ctx_hole (trm_app (trm_abs t0) t2) (t0 ^^ t2). split*.
-        subst. exists (ctx_app_2 C Val1). esplit3*.
-      subst. asserts* W (term t2). exists (ctx_app_1 C W). esplit3*.
+        subst. exists (ctx_app_2 C Val1). eauto.
+      subst. asserts* W: (term t2). exists (ctx_app_1 C W). eauto.
 Qed.
 
