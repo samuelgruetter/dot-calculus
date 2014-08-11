6e833e7b3b36907370c2d63e5051eab3253893e9
Samuel Gruetter <samuel.gruetter@epfl.ch>
2014-08-11 14:36:44 +0200

    formatting in case_skeletion

--- NoLib/case_skeleton.sh
+++ NoLib/case_skeleton.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-printf "\033c" && grep $1 -e '^  | ' | sed -e 's/ :.*/ *)>  + admit./g' | sed -e 's/  | /  (* case /g' | tr '>' '\n'
+printf "\033c" && grep $1 -e '^  | ' | sed -e 's/ :.*/ *)>    admit./g' | sed -e 's/  | /  + (* case /g' | tr '>' '\n'
 
