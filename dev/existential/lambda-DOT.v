62f1c2831a5c5fe02f0ef0916f00fa9d683ccdc1
Samuel Gruetter <samuel.gruetter@epfl.ch>
2015-01-29 13:57:02 +0100

    wip on lambda-DOT

--- dev/existential/lambda-DOT.v
+++ dev/existential/lambda-DOT.v
@@ -372,4 +372,182 @@ Definition pth2trm(p: pth): trm := match p with
   | pth_var a => trm_var a
 end.
 
+Reserved Notation "D1 && D2 == D3" (at level 40).
+Reserved Notation "D1 || D2 == D3" (at level 40).
 
+(* Not defined as a function because it's not total: Only defined if same kind of dec
+   and same label. *)
+Inductive intersect_dec: dec -> dec -> dec -> Prop :=
+| intersect_dec_typ: forall L (T1 U1 T2 U2: typ),
+    (dec_typ L T1 U1) && (dec_typ L T2 U2) == (dec_typ L (typ_or T1 T2) (typ_and U1 U2))
+| intersect_dec_fld: forall l T1 T2,
+    (dec_fld l T1) && (dec_fld l T2) == (dec_fld l (typ_and T1 T2))
+| intersect_dec_mtd: forall m T1 U1 T2 U2,
+    (dec_mtd m T1 U1) && (dec_mtd m T2 U2) == (dec_mtd m (typ_or T1 T2) (typ_and U1 U2))
+where "D1 && D2 == D3" := (intersect_dec D1 D2 D3).
+
+Inductive union_dec: dec -> dec -> dec -> Prop :=
+| union_dec_typ: forall L T1 U1 T2 U2,
+    (dec_typ L T1 U1) || (dec_typ L T2 U2) == (dec_typ L (typ_and T1 T2) (typ_or U1 U2))
+| union_dec_fld: forall l T1 T2,
+    (dec_fld l T1) || (dec_fld l T2) == (dec_fld l (typ_or T1 T2))
+| union_dec_mtd: forall m T1 U1 T2 U2,
+    (dec_mtd m T1 U1) || (dec_mtd m T2 U2) == (dec_mtd m (typ_and T1 T2) (typ_or U1 U2))
+where "D1 || D2 == D3" := (union_dec D1 D2 D3).
+
+(* on paper: G |- T ∍z D
+   in Coq: "has" returns a dec without opening it
+   but in lambda-DOT, there are no self-references anyways *)
+Inductive typ_has: ctx -> typ -> dec -> Prop :=
+(*| typ_top_has: typ_top has nothing *)
+(*| typ_bot_has: in subdec_typ, we require that Lo2 <: Lo1 <: Hi1 <: Hi2,
+      so the dec_typ that typ_bot could have, i.e.
+      (dec_typ typ_top typ_bot) is not a subdec of anything, so better say
+      typ_bot has no members! *)
+  | typ_rcd_has: forall G D,
+      typ_has G (typ_rcd D) D
+  | typ_sel_has: forall G p T L Lo Hi D,
+      ty_trm G (pth2trm p) T ->
+      typ_has G T (dec_typ L Lo Hi) ->
+      typ_has G Hi D ->
+      typ_has G (typ_sel p L) D
+  | typ_and_has_1: forall G T1 T2 D,
+      typ_has G T1 D ->
+      typ_has G (typ_and T1 T2) D
+  | typ_and_has_2: forall G T1 T2 D,
+      typ_has G T2 D ->
+      typ_has G (typ_and T1 T2) D
+  | typ_and_has_12: forall G T1 T2 D1 D2 D3,
+      typ_has G T1 D1 ->
+      typ_has G T2 D2 ->
+      D1 && D2 == D3 ->
+      typ_has G (typ_and T1 T2) D3
+  | typ_or_has: forall G T1 T2 D1 D2 D3,
+      typ_has G T1 D1 ->
+      typ_has G T2 D2 ->
+      D1 || D2 == D3 ->
+      typ_has G (typ_or T1 T2) D3
+with subtyp: ctx -> typ -> typ -> Prop :=
+  | subtyp_refl: forall G T,
+      subtyp G T T
+  | subtyp_top: forall G T,
+      subtyp G T typ_top
+  | subtyp_bot: forall G T,
+      subtyp G typ_bot T
+  | subtyp_rcd: forall G D1 D2,
+      subdec G D1 D2 ->
+      subtyp G (typ_rcd D1) (typ_rcd D2)
+  | subtyp_sel_l: forall G p X L S U T,
+      ty_trm G (pth2trm p) X ->
+      typ_has G X (dec_typ L S U) ->
+      subtyp G U T ->
+      subtyp G (typ_sel p L) T
+  | subtyp_sel_r: forall G p X L S U T,
+      ty_trm G (pth2trm p) X ->
+      typ_has G X (dec_typ L S U) ->
+      subtyp G S U -> (* <--- makes proofs a lot easier!! *)
+      subtyp G T S ->
+      subtyp G T (typ_sel p L)
+  | subtyp_and: forall G S T1 T2,
+      subtyp G S T1 ->
+      subtyp G S T2 ->
+      subtyp G S (typ_and T1 T2)
+  | subtyp_and_l: forall G T1 T2 S,
+      subtyp G T1 S ->
+      subtyp G (typ_and T1 T2) S
+  | subtyp_and_r: forall G T1 T2 S,
+      subtyp G T2 S ->
+      subtyp G (typ_and T1 T2) S
+  | subtyp_or: forall G T1 T2 S,
+      subtyp G T1 S ->
+      subtyp G T2 S ->
+      subtyp G (typ_or T1 T2) S
+  | subtyp_or_l: forall G S T1 T2,
+      subtyp G S T1 ->
+      subtyp G S (typ_or T1 T2)
+  | subtyp_or_r: forall G S T1 T2,
+      subtyp G S T2 ->
+      subtyp G S (typ_or T1 T2)
+with subdec: ctx -> dec -> dec -> Prop :=
+  | subdec_typ: forall G L Lo1 Hi1 Lo2 Hi2,
+      (* Lo2 <: Lo1 <: Hi1 <: Hi2 *)
+      subtyp G Lo2 Lo1 ->
+      subtyp G Lo1 Hi1 ->
+      subtyp G Hi1 Hi2 ->
+      subdec G (dec_typ L Lo1 Hi1) (dec_typ L Lo2 Hi2)
+  | subdec_fld: forall G l T1 T2,
+      subtyp G T1 T2 ->
+      subdec G (dec_fld l T1) (dec_fld l T2)
+  | subdec_mtd: forall G m S1 T1 S2 T2,
+      subtyp G S2 S1 ->
+      subtyp G T1 T2 ->
+      subdec G (dec_mtd m S1 T1) (dec_mtd m S2 T2)
+with ty_trm: ctx -> trm -> typ -> Prop :=
+  | ty_var: forall G x T,
+      binds x T G ->
+      ty_trm G (trm_var (avar_f x)) T
+  | ty_sel: forall G t T l U,
+      ty_trm G t T ->
+      typ_has G T (dec_fld l U) ->
+      ty_trm G (trm_sel t l) U
+  | ty_call: forall G t T m U V u,
+      ty_trm G t T ->
+      typ_has G T (dec_mtd m U V) ->
+      ty_trm G u U ->
+      ty_trm G (trm_call t m u) V
+  | ty_new: forall G ds T, (* TODO enable self reference *)
+      ty_defs G ds T ->
+      ty_trm G (trm_new ds) T
+  | ty_sbsm: forall G t T U,
+      ty_trm G t T ->
+      subtyp G T U ->
+      ty_trm G t U
+with ty_def: ctx -> def -> dec -> Prop :=
+  | ty_typ: forall G L S T,
+      subtyp G S T -> (* <----- only allow realizable bounds *)
+      ty_def G (def_typ L S T) (dec_typ L S T)
+  | ty_fld: forall G l v T,
+      ty_trm G (trm_var v) T ->
+      ty_def G (def_fld l T v) (dec_fld l T)
+  | ty_mtd: forall L m G S T t,
+      (forall x, x \notin L -> ty_trm (G & x ~ S) (open_trm x t) T) ->
+      ty_def G (def_mtd m S T t) (dec_mtd m S T)
+with ty_defs: ctx -> defs -> typ -> Prop :=
+  | ty_dsnil: forall G,
+      ty_defs G defs_nil typ_top
+  | ty_dscons: forall G ds d T D,
+      ty_defs G ds T ->
+      ty_def G d D ->
+      can_add G ds d ->
+      ty_defs G (defs_cons ds d) (typ_and T (typ_rcd D))
+with can_add: ctx -> defs -> def -> Prop :=
+  | can_add_typ: forall G ds L Lo Hi,
+      defs_hasnt ds (label_typ L) ->
+      subtyp G Lo Hi ->
+      can_add G ds (def_typ L Lo Hi)
+  | can_refine_typ: forall G ds L Lo1 Hi1 Lo2 Hi2,
+      defs_has ds (def_typ L Lo1 Hi1) ->
+      (* added dec must have good bounds: *)
+      subtyp G Lo2 Hi2 ->
+      (* and if intersected with existing bounds, still good: *)
+      subtyp G Lo1 Hi2 ->
+      subtyp G Lo2 Hi1 ->
+      can_add G ds (def_typ L Lo2 Hi2)
+  | can_add_fld: forall G ds l T x,
+      defs_hasnt ds (label_fld l) ->
+      can_add G ds (def_fld l T x)
+  | can_add_mtd: forall G ds m T1 T2 t,
+      defs_hasnt ds (label_mtd m) ->
+      can_add G ds (def_mtd m T1 T2 t).
+
+(** *** Well-formed store *)
+Inductive wf_sto: sto -> ctx -> Prop :=
+  | wf_sto_empty : wf_sto empty empty
+  | wf_sto_push : forall s G x ds T,
+      wf_sto s G ->
+      x # s ->
+      x # G ->
+      (* In lambda-DOT, ds may not use a self reference, so it's "G" and not "G & z ~ T" *)
+      ty_defs G ds T ->
+      (* No need to check if realizable bounds, because that's already done by ty_def *)
+      wf_sto (s & x ~ ds) (G & x ~ T).
