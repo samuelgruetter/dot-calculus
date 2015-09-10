e0066b69cb06eb74b67480a2ad9323bf11bf1aec
Nada Amin <namin@alum.mit.edu>
2015-09-10 22:17:28 +0200

    wf_sto lemmas

--- dev/lf/dot.v
+++ dev/lf/dot.v
@@ -516,6 +516,75 @@ Proof.
     apply* H0.
 Qed.
 
+(* ###################################################################### *)
+(** ** Well-formed store *)
+
+Lemma wf_sto_to_ok_s: forall s G,
+  wf_sto G s -> ok s.
+Proof. intros. induction H; jauto. Qed.
+
+Lemma wf_sto_to_ok_G: forall s G,
+  wf_sto G s -> ok G.
+Proof. intros. induction H; jauto. Qed.
+
+Hint Resolve wf_sto_to_ok_s wf_sto_to_ok_G.
+
+Lemma ctx_binds_to_sto_binds_raw: forall s G x T,
+  wf_sto G s ->
+  binds x T G ->
+  exists G1 G2 v, G = G1 & G2 /\ binds x v s /\ ty_trm ty_precise sub_general G1 (trm_val v) T.
+Proof.
+  introv Wf Bi. gen x T Bi. induction Wf; intros.
+  + false* binds_empty_inv.
+  + unfolds binds. rewrite get_push in *. case_if.
+    - inversions Bi. exists G (x ~ T0) v.
+      auto.
+    - specialize (IHWf _ _ Bi). destruct IHWf as [G1 [G2 [ds' [Eq [Bi' Tyds]]]]].
+      subst. exists G1 (G2 & x ~ T) ds'. rewrite concat_assoc. auto.
+Qed.
+
+Lemma invert_wf_sto_concat: forall s G1 G2,
+  wf_sto (G1 & G2) s ->
+  exists s1 s2, s = s1 & s2 /\ wf_sto G1 s1.
+Proof.
+  introv Wf. gen_eq G: (G1 & G2). gen G1 G2. induction Wf; introv Eq; subst.
+  - do 2 exists (@empty val). rewrite concat_empty_r.
+    apply empty_concat_inv in Eq. destruct Eq. subst. auto.
+  - destruct (env_case G2) as [Eq1 | [x' [T' [G2' Eq1]]]].
+    * subst G2. rewrite concat_empty_r in Eq. subst G1.
+      exists (s & x ~ v) (@empty val). rewrite concat_empty_r. auto.
+    * subst G2. rewrite concat_assoc in Eq. apply eq_push_inv in Eq.
+      destruct Eq as [? [? ?]]. subst x' T' G. specialize (IHWf G1 G2' eq_refl).
+      destruct IHWf as [s1 [s2 [Eq Wf']]]. subst.
+      exists s1 (s2 & x ~ v). rewrite concat_assoc. auto.
+Qed.
+
+Lemma sto_unbound_to_ctx_unbound: forall s G x,
+  wf_sto G s ->
+  x # s ->
+  x # G.
+Proof.
+  introv Wf Ub_s.
+  induction Wf.
+  + auto.
+  + destruct (classicT (x0 = x)) as [Eq | Ne].
+    - subst. false (fresh_push_eq_inv Ub_s).
+    - auto.
+Qed.
+
+Lemma ctx_unbound_to_sto_unbound: forall s G x,
+  wf_sto G s ->
+  x # G ->
+  x # s.
+Proof.
+  introv Wf Ub.
+  induction Wf.
+  + auto.
+  + destruct (classicT (x0 = x)) as [Eq | Ne].
+    - subst. false (fresh_push_eq_inv Ub).
+    - auto.
+Qed.
+
 Lemma typing_implies_bound: forall m1 m2 G x T,
   ty_trm m1 m2 G (trm_val (val_var (avar_f x))) T ->
   exists S, binds x S G.
