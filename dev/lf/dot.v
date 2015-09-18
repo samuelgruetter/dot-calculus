a99fdc77d7e9476ec98c71d878f9511a26bc43df
Nada Amin <nada.amin@epfl.ch>
2015-09-18 17:32:20 +0200

    narrowing

--- dev/lf/dot.v
+++ dev/lf/dot.v
@@ -519,6 +519,20 @@ Proof.
   rewrite <- EqG. assumption.
 Qed.
 
+Lemma weaken_subtyp: forall m1 m2 G1 G2 S U,
+  subtyp m1 m2 G1 S U ->
+  ok (G1 & G2) ->
+  subtyp m1 m2 (G1 & G2) S U.
+Proof.
+  intros.
+    assert (G1 & G2 = G1 & G2 & empty) as EqG. {
+    rewrite concat_empty_r. reflexivity.
+  }
+  rewrite EqG. apply* weaken_rules.
+  rewrite concat_empty_r. reflexivity.
+  rewrite <- EqG. assumption.
+Qed.
+
 (* ###################################################################### *)
 (** ** Well-formed store *)
 
@@ -1130,20 +1144,119 @@ Proof.
   unfold binds in Bis'. unfold binds in Bis. rewrite Bis' in Bis. inversion Bis.
   destruct Hex as [? [? [Bis' [Htyx EqTx]]]].
   unfold binds in Bis'. unfold binds in Bis. rewrite Bis' in Bis. inversions Bis.
-  admit.  
+  admit.
 Qed.
 
 (* ###################################################################### *)
 (** ** Narrowing *)
 
-(* TODO, define more reasonably *)
+Definition subenv(G1 G2: ctx) :=
+  forall x T2, binds x T2 G2 ->
+    binds x T2 G1 \/
+    exists T1,
+      binds x T1 G1 /\ subtyp ty_general sub_general G1 T1 T2.
 
-Lemma narrow_typing: forall G S U x t T,
-  ty_trm ty_general sub_general (G & x ~ U) t T ->
-  subtyp ty_general sub_general G S U ->
-  ty_trm ty_general sub_general (G & x ~ S) t T.
+Lemma subenv_push: forall G G' x T,
+  subenv G' G ->
+  ok (G' & x ~ T) ->
+  subenv (G' & x ~ T) (G & x ~ T).
 Proof.
-  admit.
+  intros.
+  unfold subenv. intros xb Tb Bi. apply binds_push_inv in Bi.
+  destruct Bi as [Bi | Bi].
+  + destruct Bi as [Bi1 Bi2]. subst.
+    left. eauto.
+  + destruct Bi as [Bi1 Bi2].
+    unfold subenv in H. specialize (H xb Tb Bi2). destruct H as [Bi' | Bi'].
+    * left. eauto.
+    * right. destruct Bi' as [T' [Bi1' Bi2']].
+      exists T'. split. eauto. apply weaken_subtyp. assumption. eauto.
+Qed.
+
+Lemma subenv_last: forall G x S U,
+  subtyp ty_general sub_general G S U ->
+  ok (G & x ~ S) ->
+  subenv (G & x ~ S) (G & x ~ U).
+Proof.
+  intros. unfold subenv. intros y T Bi.
+  apply binds_push_inv in Bi. destruct Bi as [Bi | Bi].
+  - destruct Bi. subst. right. exists S. split; eauto.
+    apply weaken_subtyp; eauto.
+  - destruct Bi. left. eauto.
+Qed.
+
+Lemma narrow_rules:
+  (forall m1 m2 G t T, ty_trm m1 m2 G t T -> forall G',
+    m1 = ty_general ->
+    m2 = sub_general ->
+    ok G' ->
+    subenv G' G ->
+    ty_trm m1 m2 G' t T)
+/\ (forall G d D, ty_def G d D -> forall G',
+    ok G' ->
+    subenv G' G ->
+    ty_def G' d D)
+/\ (forall G ds T, ty_defs G ds T -> forall G',
+    ok G' ->
+    subenv G' G ->
+    ty_defs G' ds T)
+/\ (forall m1 m2 G S U, subtyp m1 m2 G S U -> forall G',
+    m1 = ty_general ->
+    m2 = sub_general ->
+    ok G' ->
+    subenv G' G ->
+    subtyp m1 m2 G' S U)
+/\ (forall G T, wf_typ G T -> forall G',
+    ok G' ->
+    subenv G' G ->
+    wf_typ G' T).
+Proof.
+  apply rules_mutind; intros; eauto.
+  - (* ty_var *)
+    subst. unfold subenv in H2. specialize (H2 x T b).
+    destruct H2.
+    + eauto.
+    + destruct H as [T' [Bi Hsub]].
+      eapply ty_sub; eauto.
+  - (* ty_all_intro *)
+    subst.
+    apply_fresh ty_all_intro as y; eauto.
+    eapply H0; eauto. apply subenv_push; eauto.
+  - (* ty_new_intro *)
+    subst.
+    apply_fresh ty_new_intro as y; eauto.
+    apply H; eauto. apply subenv_push; eauto.
+    apply H0; eauto. apply subenv_push; eauto.
+  - (* ty_let *)
+    subst.
+    apply_fresh ty_let as y; eauto.
+    apply H0 with (x:=y); eauto. apply subenv_push; eauto.
+  - inversion H1 (* sub_tight *).
+  - inversion H1 (* sub_tight *).
+  - (* subtyp_bnd *)
+    subst.
+    apply_fresh subtyp_bnd as y; eauto.
+    apply H; eauto. apply subenv_push; eauto.
+  - (* subtyp_all *)
+    subst.
+    apply_fresh subtyp_all as y; eauto.
+    apply H1; eauto. apply subenv_push; eauto.
+  - (* wft_bnd *)
+    subst.
+    apply_fresh wft_bnd as y; eauto.
+    apply H; eauto. apply subenv_push; eauto.
+  - (* wft_all *)
+    subst.
+    apply_fresh wft_all as y; eauto.
+    apply H0 with (x:=y); eauto. apply subenv_push; eauto.
+Qed.
+
+Lemma narrow_typing: forall G G' t T,
+  ty_trm ty_general sub_general G t T ->
+  subenv G' G -> ok G' ->
+  ty_trm ty_general sub_general G' t T.
+Proof.
+  intros. apply* narrow_rules.
 Qed.
 
 (* ###################################################################### *)
@@ -1262,7 +1375,10 @@ Proof.
     intro Contra. inversion Contra.
     eapply narrow_typing.
     eapply H8. eauto.
+    eapply subenv_last.
     eapply tight_to_general_subtyping. assumption.
+    eapply ok_push. eapply wf_sto_to_ok_G. eassumption. eauto.
+    eapply ok_push. eapply wf_sto_to_ok_G. eassumption. eauto.
     eapply H0. eauto.
 Qed.
 
