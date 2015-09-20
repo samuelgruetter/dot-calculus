46c27d338c4e76654106eb58ee8487c80727dd15
Nada Amin <namin@alum.mit.edu>
2015-09-20 10:34:29 +0200

    subst def

--- dev/lf/dot.v
+++ dev/lf/dot.v
@@ -737,6 +737,55 @@ Proof.
 Qed.
 
 (* ###################################################################### *)
+(** ** Substitution *)
+
+Definition subst_avar (z: var) (u: var) (a: avar) : avar :=
+  match a with
+  | avar_b i => avar_b i
+  | avar_f x => If x = z then (avar_f u) else (avar_f x)
+  end.
+
+Fixpoint subst_typ (z: var) (u: var) (T: typ) { struct T } : typ :=
+  match T with
+  | typ_rcd D      => typ_rcd (subst_dec z u D)
+  | typ_and T1 T2  => typ_and (subst_typ z u T1) (subst_typ z u T2)
+  | typ_sel x L    => typ_sel (subst_avar z u x) L
+  | typ_bnd T      => typ_bnd (subst_typ z u T)
+  | typ_all T U    => typ_all (subst_typ z u T) (subst_typ z u U)
+  end
+with subst_dec (z: var) (u: var) (D: dec) { struct D } : dec :=
+  match D with
+  | dec_typ L T U => dec_typ L (subst_typ z u T) (subst_typ z u U)
+  | dec_trm L U => dec_trm L (subst_typ z u U)
+  end.
+
+Fixpoint subst_trm (z: var) (u: var) (t: trm) : trm :=
+  match t with
+  | trm_var x        => trm_var (subst_avar z u x)
+  | trm_val v        => trm_val (subst_val z u v)
+  | trm_sel x1 L     => trm_sel (subst_avar z u x1) L
+  | trm_app x1 x2    => trm_app (subst_avar z u x1) (subst_avar z u x2)
+  | trm_let t1 t2    => trm_let (subst_trm z u t1) (subst_trm z u t2)
+  end
+with subst_val (z: var) (u: var) (v: val) : val :=
+  match v with
+  | val_new T ds     => val_new (subst_typ z u T) (subst_defs z u ds)
+  | val_lambda T t   => val_lambda (subst_typ z u T) (subst_trm z u t)
+  end
+with subst_def (z: var) (u: var) (d: def) : def :=
+  match d with
+  | def_typ L T => def_typ L (subst_typ z u T)
+  | def_trm L t => def_trm L (subst_trm z u t)
+  end
+with subst_defs (z: var) (u: var) (ds: defs) : defs :=
+  match ds with
+  | defs_nil => defs_nil
+  | defs_cons rest d => defs_cons (subst_defs z u rest) (subst_def z u d)
+  end.
+
+Definition subst_ctx (z: var) (u: var) (G: ctx) : ctx := map (subst_typ z u) G.
+
+(* ###################################################################### *)
 (** ** Some Lemmas *)
 
 Lemma corresponding_types: forall G s x T,
