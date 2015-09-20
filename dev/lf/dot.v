89dabbed4a5acb3373647458d89937ca0fa2191a
Nada Amin <namin@alum.mit.edu>
2015-09-20 18:57:25 +0200

    case subtyp_bnd

--- dev/lf/dot.v
+++ dev/lf/dot.v
@@ -1221,7 +1221,23 @@ Proof.
     eapply H; eauto.
   - (* subtyp_sel2_tight *) inversion H5.
   - (* subtyp_sel1_tight *) inversion H5.
-  - admit.
+  - (* subtyp_bnd *)
+    simpl. apply_fresh subtyp_bnd as z.
+    assert (z \notin L) as FrL by eauto.
+    assert (subst_fvar x y z = z) as A. {
+      unfold subst_fvar. rewrite If_r. reflexivity. eauto.
+    }
+    rewrite <- A at 2. rewrite <- A at 3. rewrite <- A at 4.
+    rewrite <- subst_open_commute_typ. rewrite <- subst_open_commute_typ.
+    assert (subst_ctx x y G2 & z ~ subst_typ x y (open_typ z T) = subst_ctx x y (G2 & z ~ open_typ z T)) as B. {
+      unfold subst_ctx. rewrite map_concat. rewrite map_single. reflexivity.
+    }
+    rewrite <- concat_assoc. rewrite B.
+    apply H; eauto.
+    rewrite concat_assoc. reflexivity.
+    rewrite concat_assoc. apply ok_push. assumption. eauto.
+    rewrite <- B. rewrite concat_assoc. apply weaken_ty_trm. assumption.
+    apply ok_push. apply ok_concat_map. eauto. unfold subst_ctx. eauto.
   - admit.
   - admit.
   - admit.
