cfdec8dc0c2cbdf2136cd4268a1239cca551d5e4
Nada Amin <namin@alum.mit.edu>
2015-09-23 09:47:38 +0200

    update

--- dev/lf/dot.v
+++ dev/lf/dot.v
@@ -1786,6 +1786,17 @@ Proof.
   intros. apply* tight_to_general.
 Qed.
 
+Lemma shape_var_new_typing: forall m2 G s S ds x T,
+  wf_sto G s ->
+  binds x (val_new S ds) s ->
+  binds x (typ_bnd S) G ->
+  ty_trm ty_general m2 G (trm_var (avar_f x)) T ->
+  (subtyp ty_general m2 G (typ_bnd S) T \/
+   subtyp ty_general m2 G (open_typ x S) T).
+Proof.
+  admit.
+Qed.
+
 (* If G ~ s, s |- x = new(x: T)d, and G |-# x: {A: S..U} then G |-# x.A <: U and G |-# S <: x.A. *)
 Lemma tight_bound_completeness: forall G s x T ds A S U,
   wf_sto G s ->
@@ -2199,15 +2210,28 @@ Qed.
 
 (*
 Lemma (Canonical forms 1)
-
-If G ~ s and G |- v: all(x: T)U for some non-variable value v, then v is of the form lambda(x: T')t where G |- T <: T' and G, x: T' |- t: U.
-Proof: By (Possible Types), v cannot be a new because all(x: T)U is not a possible type of a new. So v must be of the form lambda(x: T')t. Again by (Possible Types) the only way a lambda(x: T')t can have a type all(x: T)U is if G |- T' <: T, G, x: T' |- t: U.
+If G ~ s and G |- x: all(x: T)U then s(x) = lambda(x: T')t where G |- T <: T' and G, x: T' |- t: U.
  *)
+Lemma canonical_forms_1: forall G s x T U,
+  wf_sto G s ->
+  ty_trm ty_general sub_general G (trm_var (avar_f x)) (typ_all T U) ->
+  (exists L T' t, binds x (val_lambda T' t) s /\ subtyp ty_general sub_general G T T' /\
+  (forall y, y \notin L -> ty_trm ty_general sub_general (G & y ~ T') (open_trm y t) (open_typ y U))).
+Proof.
+  admit.
+Qed.
 
 (*
 Lemma (Canonical forms 2)
 
-If G ~ s and G |- v: {a: T} for some non-variable value v, then v is of the form new(x: S)d for some x, S, d such that x = new(x: S)d in s, G |- d: S and d contains a definition {a = t} where G |- t: T.
+If G ~ s and G |- x: {a: T} then s(x) = new(x: S)d for some type S, definition d such that G |- d: S and d contains a definition {a = t} where G |- t: T.
 
-Proof: By (Possible Types) v cannot be a lambda because {a: T} is not a possible type of a lambda. So v must be of the form new(x: S)d. Again by (Possible Types) the only way a new(x: S)d can have a type {a: T} is if there is a definition {a = t} in d and G |- t: T.
 *)
+Lemma canonical_forms_2: forall G s x a T,
+  wf_sto G s ->
+  ty_trm ty_general sub_general G (trm_var (avar_f x)) (typ_rcd (dec_trm a T)) ->
+  (exists S ds t, binds x (val_new S ds) s /\ ty_defs G (open_defs x ds) (open_typ x S) /\ defs_has (open_defs x ds) (def_trm a t) /\ ty_trm ty_general sub_general G t T).
+Proof.
+  admit.
+Qed.
+
