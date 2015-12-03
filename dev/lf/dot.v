4a83cd1fcdab4b9b800f1cd240adb49efdd07084
Nada Amin <namin@alum.mit.edu>
2015-12-03 08:12:12 +0100

    def of has member

--- dev/lf/dot.v
+++ dev/lf/dot.v
@@ -1790,6 +1790,32 @@ Proof.
 Qed.
 
 (* ###################################################################### *)
+(** * Has member *)
+
+Inductive has_member: ctx -> var -> typ -> typ_label -> typ -> typ -> Prop :=
+| has_any : forall G x T A S U,
+  ty_trm ty_general sub_tight G (trm_var (avar_f x)) T ->
+  has_member_rules G x T A S U ->
+  has_member G x T A S U
+with has_member_rules: ctx -> var -> typ -> typ_label -> typ -> typ -> Prop :=
+| has_refl : forall G x A S U,
+  has_member_rules G x (typ_rcd (dec_typ A S U)) A S U
+| has_and1 : forall G x T1 T2 A S U,
+  has_member G x T1 A S U ->
+  has_member_rules G x (typ_and T1 T2) A S U
+| has_and2 : forall G x T1 T2 A S U,
+  has_member G x T2 A S U ->
+  has_member_rules G x (typ_and T1 T2) A S U
+| has_bnd : forall G x T A S U,
+  has_member G x (open_typ x T) A S U ->
+  has_member_rules G x (typ_bnd T) A S U
+| has_sel : forall G x y B T' A S U,
+  ty_trm ty_precise sub_general G (trm_var (avar_f y)) (typ_rcd (dec_typ B T' T')) ->
+  has_member G x T' A S U ->
+  has_member_rules G x (typ_sel (avar_f y) B) A S U
+.
+
+(* ###################################################################### *)
 (** * Misc *)
 
 (* If G ~ s, s |- x = new(x: T)d, and G |-# x: {A: S..U} then G |-# x.A <: U and G |-# S <: x.A. *)
