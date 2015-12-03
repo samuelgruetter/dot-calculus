f43f2b8eab23f827c064eac977f6dbc04e8b4a1a
Nada Amin <namin@alum.mit.edu>
2015-12-03 16:37:19 +0100

    one admit remaining in tight bound completeness

--- dev/lf/dot.v
+++ dev/lf/dot.v
@@ -2190,6 +2190,31 @@ Qed.
 (* ###################################################################### *)
 (** * Tight bound completeness *)
 
+Lemma has_member_rcd_typ_sub2_mut:
+  (forall G x T A S U,
+    has_member G x T A S U ->
+    record_type T ->
+    T = (typ_rcd (dec_typ A S U)) \/ subtyp ty_precise sub_general G T (typ_rcd (dec_typ A S U)))  /\
+  (forall G x T A S U,
+    has_member_rules G x T A S U ->
+    record_type T ->
+    T = (typ_rcd (dec_typ A S U)) \/ subtyp ty_precise sub_general G T (typ_rcd (dec_typ A S U))).
+Proof.
+  apply has_mutind; intros.
+  - apply H; eauto.
+  - left. reflexivity.
+  - inversion H0; subst. inversion H1; subst.
+    assert (record_type T1) as Htyp1. { exists ls. assumption. }
+    specialize (H Htyp1). destruct H as [H | H].
+    + subst. right. apply subtyp_and11.
+    + right.
+      eapply subtyp_trans. eapply subtyp_and11. apply H.
+  - inversion H0; subst. inversion H1; subst. inversion h; subst. inversion H3; subst.
+    right. eapply subtyp_and12.
+  - inversion H0. inversion H1.
+  - inversion H0. inversion H1.
+Qed.
+
 (* If G ~ s, s |- x = new(x: T)d, and G |-# x: {A: S..U} then G |-# x.A <: U and G |-# S <: x.A. *)
 Lemma tight_bound_completeness: forall G s x T ds A S U,
   wf_sto G s ->
@@ -2212,21 +2237,25 @@ Proof.
     + destruct Hmem as [T1' [Heq Hmem]]. inversions Heq. assumption.
     + destruct Hmem as [y [B [T' [Heq [Htyb Hmem]]]]]. inversion Heq.
   }
-  assert (exists TA, has_member G x (typ_rcd (dec_typ A TA TA)) A T1 T1) as HmemA. {
+  assert (record_type T) as Htype. {
+    eapply record_new_typing. eapply val_new_typing; eauto.
+  }
+  assert (record_type (open_typ x T)) as Htypex. {
+    apply open_record_type. assumption.
+  }
+  assert (open_typ x T = (typ_rcd (dec_typ A T1 T1)) \/ subtyp ty_precise sub_general G (open_typ x T) (typ_rcd (dec_typ A T1 T1))) as Hsub. {
+    destruct has_member_rcd_typ_sub2_mut as [HE _].
+    eapply HE; eauto.
+  }
+  assert (ty_trm ty_precise sub_general G (trm_var (avar_f x)) (open_typ x T)) as Htypx. {
     admit.
   }
-  destruct HmemA as [TA HmemA].
-  assert (TA = T1). {
-    apply has_member_inv in HmemA.
-    repeat destruct HmemA as [HmemA|HmemA].
-    + inversions HmemA. eauto.
-    + destruct HmemA as [T1' [T2' [Heq _]]]. inversion Heq.
-    + destruct HmemA as [T1' [Heq _]]. inversion Heq.
-    + destruct HmemA as [y [B [T' [Heq _]]]]. inversion Heq.
-  }
-  subst.
   assert (ty_trm ty_precise sub_general G (trm_var (avar_f x)) (typ_rcd (dec_typ A T1 T1))) as Htyp. {
-    admit.
+    destruct Hsub as [Heq | Hsub].
+    - rewrite Heq in Htypx. apply Htypx.
+    - eapply ty_sub.
+      intro. eexists. reflexivity.
+      eapply Htypx. eapply Hsub.
   }
   split.
   eapply subtyp_trans. eapply subtyp_sel1_tight. eapply Htyp. eapply Hsub2.
