152de8d38869b3ecb33899987f728f41e6ef26f5
Nada Amin <namin@alum.mit.edu>
2016-03-16 16:33:45 +0100

    wip

--- dev/lf/dot_rec.v
+++ dev/lf/dot_rec.v
@@ -280,6 +280,10 @@ Inductive ty_trm : tymode -> ctx -> ctx -> trm -> typ -> Prop :=
     ty_trm m Gs G t T ->
     subtyp m Gs G T U ->
     ty_trm m Gs G t U
+| ty_monotone_tight : forall n1 n2 Gs G t T,
+    ty_trm (ty_tight n1) Gs G t T -> n1 <= n2 ->
+    ty_trm (ty_tight n2) Gs G t T
+
 with ty_def : ctx -> ctx -> def -> dec -> Prop :=
 | ty_def_typ : forall Gs G A T,
     ty_def Gs G (def_typ A T) (dec_typ A T T)
@@ -346,7 +350,10 @@ with subtyp : tymode -> ctx -> ctx -> typ -> typ -> Prop :=
 | subtyp_bnd: forall L m Gs G T1 T2, m <> ty_precise ->
     (forall x, x \notin L ->
        subtyp m Gs (G & x ~ (typ_bnd T1)) (open_typ (in_ctx x) T1) (open_typ (in_ctx x) T2)) ->
-    subtyp m Gs G (typ_bnd T1) (typ_bnd T2).
+    subtyp m Gs G (typ_bnd T1) (typ_bnd T2)
+| subtyp_monotone_tight : forall n1 n2 Gs G S U,
+    subtyp (ty_tight n1) Gs G S U -> n1 <= n2 ->
+    subtyp (ty_tight n2) Gs G S U.
 
 Inductive wf_sto: ctx -> sto -> Prop :=
 | wf_sto_empty: wf_sto empty empty
@@ -809,77 +816,9 @@ Lemma typing_bvar_implies_false: forall m1 Gs G a T,
   ty_trm m1 Gs G (trm_var (avar_b a)) T ->
   False.
 Proof.
-  intros. remember (trm_var (avar_b a)) as t. induction H; try solve [inversion Heqt].
-  eapply IHty_trm. auto.
+  intros. remember (trm_var (avar_b a)) as t. induction H; try solve [inversion Heqt];
+  eapply IHty_trm; auto.
 Qed.
-
-(* ###################################################################### *)
-(** ** Extra Rec *)
-
-(*
-Lemma extra_bnd_rules:
-  (forall m1 m2 G t T, ty_trm m1 m2 G t T -> forall G1 G2 x S G',
-    G = G1 & (x ~ open_typ x S) & G2 ->
-    G' = G1 & (x ~ typ_bnd S) & G2 ->
-    ty_trm m1 m2 G' t T)
-/\ (forall G d D, ty_def G d D -> forall G1 G2 x S G',
-    G = G1 & (x ~ open_typ x S) & G2 ->
-    G' = G1 & (x ~ typ_bnd S) & G2 ->
-    ty_def G' d D)
-/\ (forall G ds T, ty_defs G ds T -> forall G1 G2 x S G',
-    G = G1 & (x ~ open_typ x S) & G2 ->
-    G' = G1 & (x ~ typ_bnd S) & G2 ->
-    ty_defs G' ds T)
-/\ (forall m1 m2 G J T U, subtyp m1 m2 G J T U -> forall G1 G2 x S G',
-    G = G1 & (x ~ open_typ x S) & G2 ->
-    G' = G1 & (x ~ typ_bnd S) & G2 ->
-    subtyp m1 m2 G' J T U).
-Proof.
-  apply rules_mutind; intros; eauto.
-  - (* ty_var *)
-    subst. apply binds_middle_inv in b. destruct b as [Bi | [Bi | Bi]].
-    + apply ty_var. eauto.
-    + destruct Bi as [Bin [Eqx EqT ]]. subst.
-      apply ty_rec_elim. apply ty_var. eauto.
-    + destruct Bi as [Bin [Neqx Bi]]. apply ty_var. eauto.
-  - (* ty_all_intro *)
-    subst.
-    apply_fresh ty_all_intro as y; eauto.
-    assert (y \notin L) as FrL by eauto.
-    specialize (H y FrL).
-    specialize (H G1 (G2 & y ~ T) x S).
-    eapply H; eauto.
-    rewrite concat_assoc. reflexivity.
-    rewrite concat_assoc. reflexivity.
-  - (* ty_new_intro *)
-    subst.
-    apply_fresh ty_new_intro as y; eauto;
-    assert (y \notin L) as FrL by eauto.
-    specialize (H y FrL).
-    specialize (H G1 (G2 & y ~ open_typ y T) x S).
-    eapply H; eauto.
-    rewrite concat_assoc. reflexivity.
-    rewrite concat_assoc. reflexivity.
-  - (* ty_let *)
-    subst.
-    apply_fresh ty_let as y; eauto.
-    assert (y \notin L) as FrL by eauto.
-    specialize (H0 y FrL).
-    specialize (H0 G1 (G2 & y ~ T) x S).
-    eapply H0; eauto.
-    rewrite concat_assoc. reflexivity.
-    rewrite concat_assoc. reflexivity.
-  - (* subtyp_all *)
-    subst.
-    apply_fresh subtyp_all as y; eauto.
-    assert (y \notin L) as FrL by eauto.
-    specialize (H0 y FrL).
-    specialize (H0 G1 (G2 & y ~ S2) x S).
-    eapply H0; eauto.
-    rewrite concat_assoc. reflexivity.
-    rewrite concat_assoc. reflexivity.
-Qed.
- *)
 
 (* ###################################################################### *)
 (** ** Substitution *)
