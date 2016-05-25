3c5b5dc933f590ab8b8ddc984474e0d79c8e5477
Samuel Gruetter <samuel.gruetter@epfl.ch>
2016-05-25 22:54:07 +0200

    cp MutRec.scala MutRec0.scala

---
+++ scala/miniscala1/src/main/resources/miniscala/MutRec0.scala
@@ -0,0 +1,20 @@
+object MutRec0 { z =>
+  class U // Unit
+
+  class Author {
+    def book(u: z.U): z.Book = new z.Book
+  }
+
+  class Book {
+    def author(u: z.U): z.Author = new z.Author
+  }
+
+  def run(u: z.U): z.Book = {
+    val a = (new z.Book).author(new z.U)
+    a.book(new z.U)
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(new z.Book)
+  }
+}
